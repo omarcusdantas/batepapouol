13cb1a341b37ad95c777b95db1bc873a4ffca4b5
MarcusDantas <marvinsd@gmail.com>
2023-04-15 12:16:01 -0300

    feat: add login and get messages features

---
+++ src/js/login.js
@@ -0,0 +1,54 @@
+const loginScreen = document.querySelector('.login-screen');
+const loadingScreen = document.querySelector('.loading-screen');
+const messagesScreen = document.querySelector('.messages-screen');
+let nameText;
+let statusIntervalId;
+
+axios.defaults.headers.common['Authorization'] = 'hQ1BXpUEVI2NU3TNeV1FWm1W';
+
+function toggleMessagesScreen() {
+    loadingScreen.classList.toggle('hidden');
+    messagesScreen.classList.toggle('hidden');
+}
+
+function disconnected() {
+    messagesScreen.classList.toggle('hidden');
+    loginScreen.classList.toggle('hidden');
+    alert('Desconectado do servidor');
+}
+
+function statusUpdate(data) {
+    axios.post('https://mock-api.driven.com.br/api/vm/uol/status', data)
+        .catch(disconnected);
+}
+
+function connectionSuccess(data) {
+    statusIntervalId = setInterval(statusUpdate, 5000, data);
+    toggleMessagesScreen();
+    setInterval(getMessages,3000);
+}
+
+function toggleLoginScreen() {
+    loginScreen.classList.toggle('hidden');
+    loadingScreen.classList.toggle('hidden');
+}
+
+function connectionError(error) {
+    if (error.response.status === 400) {
+        toggleLoginScreen();
+        alert('Há um usuário com esse nome logado, escolha outro nome.');
+    }
+}
+
+function connect() {
+    nameText = loginScreen.querySelector('input').value;
+    toggleLoginScreen();
+
+    const data = {
+        name: nameText
+    }
+
+    axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', data)
+        .then(connectionSuccess(data))
+        .catch(error => connectionError(error));    
+}
---
+++ src/js/manageMessages.js
@@ -0,0 +1,40 @@
+function renderMessages(messages) {
+    const messagesList = document.querySelector('.messages-container').querySelector('ul');
+    messagesList.innerHTML = '';
+
+    messages.forEach((message) => {
+        if (message.type === 'private_message' && message.from === nameText) {
+            messagesList.innerHTML += `
+                <li class="message-private">
+                    <h3 class="message-content"><span class="message-time">(${message.time})</span><strong>${message.from}</strong>
+                    reservadamente para<strong>${message.to}:</strong>${message.text}</h3>
+                </li>
+            `;
+        }
+        else if (message.type === 'message') {
+            messagesList.innerHTML += `
+                <li class="message-all">
+                    <h3 class="message-content"><span class="message-time">(${message.time})</span><strong>${message.from}</strong>
+                    para<strong>${message.to}:</strong>${message.text}</h3>
+                </li>
+            `;
+        }
+        else if (message.type === 'status') {
+            messagesList.innerHTML += `
+                <li class="message-status">
+                    <h3 class="message-content"><span class="message-time">(${message.time})</span><strong>${message.from}</strong>${message.text}</h3>
+                </li>
+            `;
+        }
+    });
+
+    const allMessages = messagesList.querySelectorAll('li');
+    const lastMessage = allMessages[allMessages.length-1];
+    lastMessage.scrollIntoView();
+}
+
+function getMessages() {
+    axios.get('https://mock-api.driven.com.br/api/vm/uol/messages')
+        .then(response => renderMessages(response.data))
+        .catch(error => console.log(error));
+}
